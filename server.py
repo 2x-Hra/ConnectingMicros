4e334ed0224573d1eb7849ff2321fd6a95852d27
Hossein2x <hrakbari77@gmail.com>
2020-01-23 11:10:46 +0330

    port checked

--- server.py
+++ server.py
@@ -1,18 +1,8 @@
 import serial 
 
-ser = serial.Serial(port= 'COM1', baudrate=9600,bytesize=serial.EIGHTBITS,timeout=2)
+ser = serial.Serial(port= 'COM1', baudrate=9600)
 
-try:
-    ser.isOpen()
-except:
-    print("error")
-    exit()
-if(ser.isOpen()):
-    try:
-        while(1):
-            if(ser.inWaiting()>0):
-                print (ser.readline())
-            else:
-                ser.write(b'1')
-    except Exception as e:
-        print (e)
+print(ser.name)
+ser.write(b'hello')
+
+print(ser.is_open)
