8af267ed2650e8e97ca60a6be544fef0dce92e91
Hossein2x <hrakbari77@gmail.com>
2020-01-24 18:38:34 +0330

    Security :)

--- server.py
+++ server.py
@@ -1,20 +1,7 @@
 import serial 
 import time
 
-ser = serial.Serial(port= 'COM2', baudrate=9600)
-
-print(ser.name)
-while(True):
-
-    temp = input("000 Or MOTOR speed :")
-    ser.write(str.encode(temp))
-    if(temp == "000f"):
-        temp_analog = str(ser.read(2))
-        temp_list = list(temp_analog)
-        temp_list.pop(1)
-        temp_list.pop(len(temp_list)-1)
-        temp_list.pop(0)
-        print(''.join(temp_list))
+# SECURITY
   
    
 # 000f
